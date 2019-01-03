9d75651f8ef30e533d67dd2a29e126cf51405534
Ryan Gill <rdgill95@gmail.com>
2019-01-03 23:01:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-  programmer_hash[yukihiro_matsumoto]
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  programmer_hash << :yukihiro_matsumoto
 end
 
 def changing_alan
